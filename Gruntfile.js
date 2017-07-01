68e4441c8fedd6a43fd4b37966c7a6ca050f7d42
koko-ng <koko-ng@users.noreply.github.com>
2017-07-01 23:09:10 +0200

    new design #01

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,21 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				files: {
+					'./src/css/font-awesome/font-awesome.css': './src/sass/font-awesome/font-awesome.scss',
+					'./src/css/palet.css': './src/sass/palet.sass',
+					'./src/css/style.css': './src/sass/style.sass',
+					'./src/css/beer.css': './src/sass/beer.sass',
+					'./src/css/account/validation.css': './src/sass/account/validation.sass'
+
+				}
+			}
+		},
 		requirejs: {
 			compile: {
 				options: {
@@ -29,8 +44,9 @@ module.exports = function(grunt) {
 	// Load the plugin that provides the "requirejs" task.
 	grunt.loadNpmTasks('grunt-bower-task');
 	grunt.loadNpmTasks('grunt-contrib-requirejs');
+	grunt.loadNpmTasks('grunt-sass');
 
 	// Default task(s).
-	grunt.registerTask('default', ['bower:install', 'requirejs']);
+	grunt.registerTask('default', ['sass', 'bower:install', 'requirejs']);
 
 };
